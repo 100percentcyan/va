5107562e32b07732ad396a93c5a3ac44314f8f93
100percentcyan <109283363+100percentcyan@users.noreply.github.com>
2023-02-03 17:17:51 +0600

    Add files via upload

--- va_init.sh
+++ va_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare -a vo
-BASIC_COLOR=$YELLOW
-default="-" && arrow="*->" && small_arrow="*>" && drift_lights="$MAGENTA-*-" && tri_lines="$WHITE---" && blue_ellipses="$BLUE..."
+BASIC_COLOR=$YELLOW && BG_BASIC_COLOR=$NO_COLOR
+default="-" && arrow="*->" && small_arrow="*>" && drift_lights="$MAGENTA-*-" && tri_lines="$WHITE---" && blue_ellipses="$BLUE..." && green_ellipses="$GREEN..." && geo_comma="$BLUE,$GREEN,"
 if [[ ${vo[9]} != "constant" ]]; then
 vo[8]=$default
 fi
@@ -10,6 +10,29 @@ vo[10]="."
 fi
 vo[3]=${vo[8]}
 
-function sas {
+function sws {
+          echo "#!/bin/bash" >> ${vo[12]}
           echo ". va.sh ${vo[11]}" >> ${vo[12]}
+}
+
+function swg {
+          echo "vo[8]='${vo[8]}'" >> ${vo[12]}
+          echo "vo[9]='constant'" >> ${vo[12]}
+}
+function swv {
+          echo "vo[10]=${vo[10]}" >> ${vo[12]}
+}
+
+function swr {
+          echo "vo[4]=${vo[4]}" >> ${vo[12]}
+          echo "vo[5]=${vo[5]}" >> ${vo[12]}
+}
+function swrs {
+          echo "vo[6]='${vo[6]}'" >> ${vo[12]}
+}
+function swa {
+          echo ${vo[13]} >> ${vo[12]}
+}
+function ra {
+          sas && swc && swv && swr && swrs && swa
 }
